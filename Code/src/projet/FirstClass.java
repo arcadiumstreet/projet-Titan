93a86dcd1301ad8804eac746e256c9f94a608f0f
Calu12 <coemgen.genevey@gmail.com>
2022-09-26 17:09:31 +0200

    test

--- Code/src/projet/FirstClass.java
+++ Code/src/projet/FirstClass.java
@@ -24,6 +24,23 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		Robot pierrot = new Robot(MotorPort.B,MotorPort.C,MotorPort.A,SensorPort.S2,SensorPort.S3);
+	
+		
+		pierrot.getUltrasonics().getDistance().fetchSample(pierrot.getUltrasonics().getSample(), 0);
+		float dist = pierrot.getUltrasonics().getSample()[0];
+		pierrot.getRightGear().rotate(360, true);
+		int i = 0;
+		while (Math.abs(pierrot.getUltrasonics().getSample()[0] - dist) < 0.1 && i < 30) {
+			dist = pierrot.getUltrasonics().getSample()[0];
+			
+			i++;
+		}
+		pierrot.getRightGear().stop();
+		if(i<30)
+			pierrot.catchTarget((int) (pierrot.getUltrasonics().getSample()[0]));
+		
+		
+		
 		
 		//pierrot.turn90Degres(pierrot.LEFT);
 		//pierrot.turn90Degres(pierrot.RIGHT);
@@ -34,17 +51,35 @@ public class FirstClass {
 		
 		//pierrot.moveCm(pierrot.FRONT, 50);
 		
-		pierrot.moveCm(pierrot.BACK, 10);
+		//pierrot.moveCm(pierrot.BACK, 10);
  
-		pierrot.Research(pierrot.RIGHT);
+		//pierrot.Research(pierrot.RIGHT);
 		
-		pierrot.moveCm(pierrot.FRONT, 10);
+		//pierrot.moveCm(pierrot.FRONT, 10);
 		/*pierrot.getUltrasonics().getDistance().fetchSample(pierrot.getUltrasonics().getSample(), 0);
 		if(pierrot.getUltrasonics().objectDetectedInRange(100))
 			pierrot.catchTarget((int)(pierrot.getUltrasonics().getSample()[0] * 100));
 		Delay.msDelay(1000);*/
 		// attrapper un objet sans exception
 		
+		
+		/*pierrot.getUltrasonics().getDistance().fetchSample(pierrot.getUltrasonics().getSample(), 0);
+		float dist = pierrot.getUltrasonics().getSample()[0];
+		int i = 0;
+		while (Math.abs(pierrot.getUltrasonics().getSample()[0] - dist) < 0.3 && i < 30) {
+			dist = pierrot.getUltrasonics().getSample()[0];
+			pierrot.littleTurn(pierrot.RIGHT);
+			i++;
+			// pierrot.catchTarget((int)(pierrot.getUltrasonics().getSample()[0] * 100));
+		}
+		if (pierrot.getUltrasonics().getSample()[0] - dist < 0)
+			pierrot.catchTarget((int) (pierrot.getUltrasonics().getSample()[0]));
+		else {
+			pierrot.littleTurn(pierrot.LEFT);
+			pierrot.catchTarget((int) (pierrot.getUltrasonics().getSample()[0]));
+		}*/
+		//Delay.msDelay(1000);
+		// tourne et attrapper un objet si detecté(non testé)
 		
 		
 	}
