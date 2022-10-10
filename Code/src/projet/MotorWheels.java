357f09736bb535c0cca50489758c5134978ff124
Calu12 <coemgen.genevey@gmail.com>
2022-10-10 16:48:24 +0200

    recuperation de code

--- Code/src/projet/MotorWheels.java
+++ Code/src/projet/MotorWheels.java
@@ -4,21 +4,30 @@ import lejos.hardware.motor.Motor;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
+import lejos.hardware.port.Port;
+
 
 public class MotorWheels {
 	private Wheel moteur1 ;
 	private Wheel moteur2 ; 
 	private Chassis chassis ; 
 	 
-	public MotorWheels() {
+	public MotorWheels(Port A,Port D) {
 		Wheel moteur1 = WheeledChassis.modelWheel(Motor.A, 81.6).offset(-70);
 		Wheel moteur2 = WheeledChassis.modelWheel(Motor.D, 81.6).offset(70);
 		chassis = new WheeledChassis(new Wheel[]{ moteur1, moteur2 }, WheeledChassis.TYPE_DIFFERENTIAL);
 	}
 	
-	public void avance() {
-		chassis.isMoving();
+	public boolean enMouvement() {
+		return chassis.isMoving();
 	}
+	
+	
+	
+	public void avance() {
+		
+	}
+	
 	public void tournedroit() {
 		chassis.getLinearDirection();
 	}
