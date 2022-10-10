f3c50f3e329555bd36074ec7dea59b335ef29dd0
chocn <noe.choc@etu.univ-grenoble-alpes.fr>
2022-10-10 16:05:22 +0200

    test

--- Code/src/projet/Motor.java
+++ Code/src/projet/Motor.java
@@ -1,17 +1,16 @@
 package projet;
 
-import lejos.hardware.motor.Motor;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 
-public class motor {
+public class Motor {
 	private Wheel moteur1 ;
 	private Wheel moteur2 ; 
 	private Chassis chassis ; 
 	 
 	
-	public motor() {
+	public Motor() {
 		Wheel moteur1 = WheeledChassis.modelWheel(Motor.A, 81.6).offset(-70);
 		Wheel moteur2 = WheeledChassis.modelWheel(Motor.D, 81.6).offset(70);
 		chassis = new WheeledChassis(new Wheel[]{ moteur1, moteur2 }, WheeledChassis.TYPE_DIFFERENTIAL);
